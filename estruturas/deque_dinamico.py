381e4b7bdba5404536a81ebaa59333a45ef5c9a5
Cristian Madeira de Souza Pereira <cristian.pereira@aluno.cefet-rj.br>
2020-11-10 14:28:07 -0300

    Faz refatorações guiada por testes .

--- estruturas/deque_dinamico.py
+++ estruturas/deque_dinamico.py
@@ -1,98 +1,118 @@
-#
-# Utilizaremos a classe Lista para definir as principais funções 
-# da estrutura linear sem restrições.
-#
 class DequeDinamico:
-    
-    def __init__(self):
+
+    def __init__(self, limite):
         self.inicio = None
-        
-    def insere_esq(self, elemento):
+        self.fim = None
+        self.tamanho = 0
+        self.limite = limite
+
+    def insere_na_esquerda(self, elemento):
         node = Node(elemento)
-        if(self.inicio is None):
+        if self.inicio is None:
             self.inicio = node
+            self.fim = node
         else:
             node.proximo = self.inicio
             self.inicio = node
+        self.tamanho = self.tamanho + 1
         return node
-        
-    def insere_dir(self, elemento):
+
+    def insere_na_direita(self, elemento):
         node = Node(elemento)
-        if(self.inicio is None):
+        if self.inicio is None:
             self.inicio = node
+
         else:
-            ult_node = self.inicio
-            # procurar o ultimo no da lista
-            while (ult_node.proximo is not None):
-                ult_node = ult_node.proximo
-            ult_node.proximo = node
+            node.anterior = self.fim
+            self.fim.proximo = node
+        self.fim = node
+        self.tamanho = self.tamanho + 1
         return node
-    
-    def remove_esq(self):
-        node = self.inicio
-        if (node is not None):
-            self.inicio = node.proximo
-            del node
-            return self.inicio
-        return False
-    
-    def remove_dir(self):
-        node_anterior = self.inicio
-        if (node_anterior.proximo is None):
-            self.inicio = None
-            del node_anterior
+
+    def remove_na_esquerda(self):
+        if self.inicio is not None:
+            element = self.inicio
+            self.inicio = element.proximo
+            return element
         else:
-            node = self.inicio.proximo
-        # percorrer toda lista
-        while(node.proximo is not None):
-            node_anterior = node
-            node = node.proximo
-        del node
-        node_anterior.proximo = None
-        return self.inicio
-            
+            Exception('Lista Vazia!')
+
+    def remove_na_direita(self):
+        if self.fim is not None:
+            element = self.fim
+            self.fim = element.anterior
+            return element
+        else:
+            Exception('Lista vazia!')
+
     def ordena(self):
-        #implemente a ordenação da lista
-        return False
-    
+        elements = self.all()
+        for i in range(self.get_tamanho()-1):
+            j = i + 1
+            while j < self.tamanho:
+                if elements[i] > elements[j]:
+                    elements[i], elements[j] = self.troca(
+                        elements[i], elements[j])
+                j = j + 1
+            i = i + 1
+        return elements
+
+    def troca(self, var1, var2):
+        aux = var1
+        var1 = var2
+        var2 = aux
+        return var1, var2
+
     def pesquisa(self, elemento):
-        node = self.inicio
-        while (node is not None):
-            chave = str(node.conteudo)
-            if (chave == str(elemento)):
-                return node
-            node = node.proximo
-        return "Elemento não encontrado"
-    
-    # Para permitir o usuário acompanhar o tamanho da
-    # lista, defini um método de apoio get_tamanho
+        elements = self.ordena()
+        esquerda = 0
+        direita = self.get_tamanho() - 1
+        while(esquerda <= direita):
+            meio = int((direita + esquerda)/2)
+            if elemento == elements[meio]:
+                return elemento
+            elif elemento < elements[meio]:
+                direita = meio - 1
+            else:
+                esquerda = meio + 1
+        Exception('Elemento não encontrado!')
+
     def get_tamanho(self):
-        node = self.inicio
-        tamanho = 0
-        while(node is not None):
-            tamanho += 1
-            node = node.proximo
-        return tamanho
-    
+        return self.tamanho
+
+    def get_limite(self):
+        return self.limite
+
     def vazia(self):
         return (self.inicio is None)
-        
-    def mostra(self):
-        if(self.inicio is None):
-            print("Lista vazia")
+
+    def all(self):
+        if self.inicio is None:
+            Exception('Lista vazia!')
         else:
-            node = self.inicio
-            lista_str = ""
-            while (node is not None):
-                lista_str += str(node.conteudo) + " >> "
-                node = node.proximo
-            print(lista_str.rstrip(' >> '))
+            elements = []
+            element = self.inicio
+            while element is not None:
+                elements.append(element.conteudo)
+                element = element.proximo
+            return elements
+
+    def get_ultimo_elemento(self):
+        return self.fim
+
+    def get_primeiro_elemento(self):
+        return self.inicio
+
 
 class Node:
+    proximo = None
+    anterior = None
+    conteudo = None
+
     def __init__(self, valor):
         self.proximo = None
         self.anterior = None
         self.conteudo = valor
-        
+
     def __str__(self):
         return str(self.conteudo)
---
+++ estruturas/tests/deque_dinamico.py
@@ -0,0 +1,156 @@
+import unittest
+from deque_dinamico import DequeDinamico
+
+
+class DequeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.limite = 10
+        self.deque = DequeDinamico(self.limite)
+
+    def test_get_tamanho(self):
+        expected = 0
+        result = self.deque.get_tamanho()
+        self.assertEqual(expected, result)
+
+    def test_get_limite(self):
+        expected = 10
+        self.assertEqual(expected, self.limite)
+
+    def test_insere_na_direita(self):
+        element1 = 'CEFET/RJ'
+        result1 = self.deque.insere_na_direita(element1)
+        expected1 = 'CEFET/RJ'
+        self.assertEqual(expected1, result1.conteudo)
+
+        element2 = 'Maria da Graça'
+        result2 = self.deque.insere_na_direita(element2)
+        expected2 = 'Maria da Graça'
+        self.assertEqual(expected2, result2.conteudo)
+
+        expected_tamanho = 2
+        result_tamanho = self.deque.get_tamanho()
+
+        self.assertEqual(expected_tamanho, result_tamanho)
+
+    def test_get_ultimo_elemento(self):
+
+        elemento1, elemento2 = 'CEFET/RJ', 'Maria da Graça'
+
+        deque = DequeDinamico(3)
+        deque.insere_na_direita(elemento1)
+        deque.insere_na_direita(elemento2)
+
+        result = deque.get_ultimo_elemento()
+        expected = elemento2
+
+        self.assertEqual(expected, result.conteudo)
+
+    def test_insere_na_esquerda(self):
+        elemento1, elemento2 = 'CEFET/RJ', 'Maria da Graça'
+
+        deque = DequeDinamico(3)
+        deque.insere_na_esquerda(elemento1)
+        deque.insere_na_esquerda(elemento2)
+
+        result = deque.get_tamanho()
+        expected = 2
+
+        self.assertEqual(expected, result)
+
+    def test_get_primeiro_elemento_inserindo_na_esquerda(self):
+        elemento1, elemento2 = 'CEFET/RJ', 'Maria da Graça'
+
+        deque = DequeDinamico(3)
+        deque.insere_na_esquerda(elemento1)
+        deque.insere_na_esquerda(elemento2)
+
+        result = deque.get_primeiro_elemento()
+        expected = elemento2
+
+        self.assertEqual(expected, result.conteudo)
+
+    def test_get_ultimo_elemento_inserindo_na_esquerda(self):
+        elemento1, elemento2 = 'CEFET/RJ', 'Maria da Graça'
+
+        deque = DequeDinamico(3)
+        deque.insere_na_esquerda(elemento1)
+        deque.insere_na_esquerda(elemento2)
+
+        result = deque.get_ultimo_elemento()
+        expected = elemento1
+
+        self.assertEqual(expected, result.conteudo)
+
+    def test_get_primeiro_elemento_inserindo_na_direita(self):
+        elemento1, elemento2 = 'CEFET/RJ', 'Maria da Graça'
+
+        deque = DequeDinamico(3)
+        deque.insere_na_direita(elemento1)
+        deque.insere_na_direita(elemento2)
+
+        result = deque.get_primeiro_elemento()
+        expected = elemento1
+
+        self.assertEqual(expected, result.conteudo)
+
+    def test_get_ultimo_elemento_inserindo_na_direita(self):
+        elemento1, elemento2 = 'CEFET/RJ', 'Maria da Graça'
+
+        deque = DequeDinamico(3)
+        deque.insere_na_direita(elemento1)
+        deque.insere_na_direita(elemento2)
+
+        result = deque.get_ultimo_elemento()
+        expected = elemento2
+
+        self.assertEqual(expected, result.conteudo)
+
+    def test_remove_na_esquerda(self):
+        elemento1, elemento2 = 'CEFET/RJ', 'Maria da Graça'
+
+        deque = DequeDinamico(3)
+        deque.insere_na_direita(elemento1)
+        deque.insere_na_direita(elemento2)
+
+        result = deque.remove_na_esquerda()
+        expected = elemento1
+
+        self.assertEqual(expected, result.conteudo)
+
+    def test_remove_na_direita(self):
+        elemento1, elemento2 = 'CEFET/RJ', 'Maria da Graça'
+
+        deque = DequeDinamico(3)
+        deque.insere_na_direita(elemento1)
+        deque.insere_na_direita(elemento2)
+
+        result = deque.remove_na_direita()
+        expected = elemento2
+
+        self.assertEqual(expected, result.conteudo)
+
+    def test_ordenar(self):
+        test_sorted_list = ['a', 'b', 'c', 'd']
+
+        deque = DequeDinamico(3)
+        deque.insere_na_direita('d')
+        deque.insere_na_direita('c')
+        deque.insere_na_direita('b')
+        deque.insere_na_direita('a')
+
+        self.assertEqual(test_sorted_list, deque.ordena())
+
+    def test_pesquisa(self):
+        elemento = 'b'
+
+        deque = DequeDinamico(3)
+        deque.insere_na_direita('d')
+        deque.insere_na_direita('c')
+        deque.insere_na_direita('b')
+        deque.insere_na_direita('a')
+
+        expected = 'b'
+        result = deque.pesquisa(elemento)
+
+        self.assertEqual(expected, result)
